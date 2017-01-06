26fc82f005092c5e799fa94d40f239bf2526be18
Aaro Videvik <videvik@greeny.cs.tlu.ee>
2017-01-06 12:40:08 +0200

    3.kodutöö

--- User.class.php
+++ User.class.php
@@ -1,69 +1,81 @@
-<?php
+<?php 
 class User {
 	
+	//private $name = "Romil";
+	//public $familyName = "Robtsenkov";
 	private $connection;
 	
-
+	// käivitatakse siis kui new ja see mis saadekse
+	//sulgudesse new User(?) see jõuab siia
 	function __construct($mysqli){
 		
+		// this viitab sellele klassile siin
+		// selle klassi muutuja connection
 		$this->connection = $mysqli;
-		
 	}
 	
-	function signup($email, $password, $gender, $birthdate) {
-			
-			$stmt = $this->connection->prepare("INSERT INTO mvp_user (email, password, gender, birthdate) VALUE (?, ?, ?, ?)");
-			echo $this->connection->error;
-			
-			$stmt->bind_param("ssss", $email, $password, $gender, $_POST["birthdate"]);
-			
-			if ($stmt->execute() ){
-				echo "õnnestus";
-			} else {
-				echo "ERROR".$stmt->error;
-			}
-		}
+	/* KLASSI FUNKTSIOONID */
 	
 	function login($email, $password) {
 		
 		$notice = "";
+				
+		$stmt = $this->connection->prepare("
 		
-			$stmt = $this->connection->prepare("SELECT id, email, password, created, gender, birthdate FROM mvp_user WHERE email = ?");
+			SELECT id, email, password, created
+			FROM user_sample
+			WHERE email = ?
+		
+		");
+		// asendan ?
+		$stmt->bind_param("s", $email);
+		
+		// määran muutujad reale mis kätte saan
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		
+		$stmt->execute();
+		
+		// ainult SLECTI'i puhul
+		if ($stmt->fetch()) {
 			
-			echo $this->connection->error;
-			
-			$stmt->bind_param("s", $email);
-			
-			$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created, $gender, $birthdate);
-			
-			$stmt->execute();
-			
-			if($stmt->fetch()) {
+			// vähemalt üks rida tuli
+			// kasutaja sisselogimise parool räsiks
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb) {
+				// õnnestus 
+				echo "Kasutaja ".$id." logis sisse";
 				
-				$hash = hash("sha512", $password);
-				if ($hash == $passwordFromDb){
-					echo "Kasutaja $id logis sisse";
-					
-					$_SESSION["userId"] = $id;
-					$_SESSION["userEmail"] = $emailFromDb;
-					
-					header("Location: data.php");
-					
-					} else {
-						$notice = "parool vale";
-				}
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				header("Location: data.php");
+				exit();
 			} else {
-				
-				$notice = "Sellise emailiga ".$email." kasutajat ei ole olemas";
+				$notice = "Vale parool!";
 			}
 			
-			return $notice;
-			
+		} else {
+			// ei leitud ühtegi rida
+			$notice = "Sellist emaili ei ole!";
+		}
+		
+		return $notice;
+	}
+	
+	function signup($email, $password) {
+		
+		$stmt = $this->connection->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		echo $this->connection->error;
+		
+		$stmt->bind_param("ss", $email, $password );
+
+		if ( $stmt->execute() ) {
+			echo "salvestamine õnnestus";	
+		} else {	
+			echo "ERROR ".$stmt->error;
+		}
 		
 	}
 	
-	
-	
-	
-}
+} 
 ?>
